817d85f9126f013ced6f1172458e34ed597446c8
manampy_abraham <abraham.dev89@gmail.com>
2025-06-09 21:30:52 +0200

    test-me

---
+++ frontend/frontend/cypress/e2e/auth.cy.js
@@ -0,0 +1,21 @@
+describe("Connexion utilisateur", () => {
+  it("affiche un message si les identifiants sont incorrects", () => {
+    cy.visit("http://localhost:8080/#/login");
+
+    cy.get('[data-cy="login-input-username"]').type("test2@test.fr");
+    cy.get('[data-cy="login-input-password"]').type("mauvaismotdepasse");
+    cy.get('[data-cy="login-submit"]').click();
+
+    cy.get('[data-cy="login-errors"]')
+      .should("be.visible")
+      .and("contain", "Identifiants incorrects");
+  });
+
+  it("affiche un message si les identifiants sont corrects", () => {
+    cy.visit("http://localhost:8080/#/login");
+
+    cy.get('[data-cy="login-input-username"]').type("test2@test.fr");
+    cy.get('[data-cy="login-input-password"]').type("testtest");
+    cy.get('[data-cy="login-submit"]').click();
+  });
+});
